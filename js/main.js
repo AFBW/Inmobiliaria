12a6c04f3712f2096b54059c38136214c1709f80
Alejandro Fernandez <alexnavalarchitect@gmail.com>
2022-06-21 22:33:17 +0200

    My final commit

--- js/main.js
+++ js/main.js
@@ -1,23 +1,20 @@
-$(document).ready(function () {
-    //Cambio de header
-    $(window).on('scroll', function () {
-        let position = $('#pr').offset();
-        if ($(window).scrollTop() >= position.top) {
-            $('header').removeClass('transparent');
-        }
-        else {
-            $('header').addClass('transparent');
+$(document).ready(function() {
+    // Cambio de header
+    $(window).on('scroll', function() {
+        let posicion = $('#pr').offset();
+        if ($(window).scrollTop() >= posicion.top) {
+            $('header').removeClass('transparente');
+        } else {
+            $('header').addClass('transparente');
         }
     })
-
-    //Accionamiento del menu lateral
-    $('#burger').click(function () {
+    // Accionamiento del menú lateral
+    $('#burger').click(function() {
         $('#burger').toggleClass('abierto');
         $('.menu').toggleClass('abierto');
     })
-
-    //Smooth Scroll
-    $('a').on('click', function () {
+    // Smooth scroll
+    $('a').on('click', function() {
         if (this.hash !== '') {
             let hash = this.hash;
             $('html, body').animate({
